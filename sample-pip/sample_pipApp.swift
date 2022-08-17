315e678e5a446757cd8ce07290cbf6e490c99c7b
rtsuzuki <rtsuzuki@yahoo-corp.jp>
2022-08-18 03:28:59 +0900

    Initial Commit

---
+++ sample-pip/sample_pipApp.swift
@@ -0,0 +1,17 @@
+//
+//  sample_pipApp.swift
+//  sample-pip
+//
+//  Created by rtsuzuki on 2022/08/18.
+//
+
+import SwiftUI
+
+@main
+struct sample_pipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
